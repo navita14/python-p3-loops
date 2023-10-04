a4986d679f138605fde1d5b389dbc0c156bd4f87
Navita Ramasar <navitaramasar14@gmail.com>
2023-10-04 15:39:27 -0400

    finished

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 0
+    while i <= 10:
+        print(i)
+        print("Happy New Year!")
+        i += 1
+
+happy_new_year()
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_nums = []
+    for nums in int_list:
+        squared_nums.append(nums ** 2)
+    return squared_nums
+
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1,101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+    
